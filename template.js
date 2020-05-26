00c6521f04b13ae3242162e2a775db2b980eb9ca
chencheng <oc_co@qq.com>
2020-05-26 16:01:26 +0800

    优化模板代码

--- template.js
+++ template.js
@@ -1,54 +1,63 @@
 /**
- * pages模版快速生成脚本,执行命令 npm run page `文件名`
- * components模版快速生成脚本,执行命令 npm run comp `文件名`
+ * pages模版快速生成脚本,执行命令 npm run page `文件名` [s]
+ * components模版快速生成脚本,执行命令 npm run comp `文件名` [s]
  */
 
 const fs = require('fs');
-const cwd = process.cwd();
-const tmplName = process.argv[2];
-const dirName = process.argv[3];
-const isStore = process.argv[4];
-console.log(process.cwd())
-if (!dirName) {
+const cwd = process.cwd(); // 项目路径
+const cmd = process.argv[2];  // npm run 命令
+const tmplName = process.argv[3]; // 模板名
+const isStore = process.argv[4] === 's'; // 是否创建 store
+const isComp = cmd === 'comp';
+let cmdDir = cmd === 'page' ? 'pages' : 'components';
+let suffix = cmd === 'page' ? 'page' : 'oo';
+
+let obj = {
+  fnName: isComp ? `${titleCase(suffix)}${titleCase(tmplName)}` : titleCase(tmplName), // hook函数名
+  cssName: `${suffix}-${tmplName}`, // css样式名
+  tmplName: isComp ? `${suffix}-${tmplName}` : `${tmplName}` // 创建模板的名字
+}
+
+if (!tmplName) {
   console.log('文件夹名称不能为空！');
   console.log('示例：npm run page test');
   process.exit(0);
 }
-let suffix = tmplName === 'page' ? 'page' : 'oo'
-let fnName = tmplName === 'page' ? titleCase(dirName) : 'Oo' + titleCase(dirName)
+
 // 页面模版
 let indexTmpl = `import Taro from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
-${isStore === 's' ? 'import {useStore} from \'../../store/demo\'' : ''}
+import { View } from '@tarojs/components'
+${isStore ? `import { useStore } from '../../store/${tmplName}'` : ''}
 import './index.scss'
 
-export default ${fnName} = () => {
-  ${isStore === 's' ? 'const [state,actions] = useStore()' : ''}
+export default ${obj.fnName} = () => {
+  ${isStore ? 'const [state, actions] = useStore()' : ''}
   return (
-    <View className="${suffix}-${dirName}" ${isStore === 's' ? 'onClick={()=> actions.setName(\'kK .\')}' : ''}>
-      ${dirName}${isStore === 's' ? '{state.name}' : ''}
+    <View className="${obj.cssName}" ${isStore ? 'onClick={()=> actions.setName(\'kK .\')}' : ''}>
+      hello,${obj.fnName}${isStore ? '，这是来自远方的{state.name}' : ''}
     </View>
   )
 }
 
-${fnName}.config = {
-  navigationBarTitleText: '${dirName}'
+${obj.fnName}.config = {
+  navigationBarTitleText: '${obj.fnName}'
 }
 
 `;
-if (tmplName === 'comp') {
-  indexTmpl += `${fnName}.options = {
-    addGlobalClass: true
-  }`
+if (isComp) {
+  indexTmpl += `${obj.fnName}.options = {
+  addGlobalClass: true
+}
+
+  `
 }
 // scss文件模版
 const scssTmpl = `@import "../../var.scss";
-.${suffix}-${dirName} {
-}
+.${obj.cssName} {}
 `;
 // store文件模版
 const storeTmpl = `const sleep = async t => new Promise(resolve => setTimeout(resolve, t))
-const initialState = { name: '${dirName}' }
+const initialState = { name: '${tmplName}' }
 const actions = {
     async setInfo () {
         await sleep(3000)
@@ -62,34 +71,34 @@ const actions = {
     }
 }
 
-const ${dirName} = new Model({
+const ${tmplName} = new Model({
     initialState,
     actions
 })`;
 
-let dir = tmplName === 'page' ? 'pages' : 'components'
-let fileName = tmplName === 'page' ? '' : 'oo-'
-fs.mkdirSync(`${cwd}/src/${dir}/${fileName}${dirName}`);
-process.chdir(`${cwd}/src/${dir}/${fileName}${dirName}`);
+
+fs.mkdirSync(`${cwd}/src/${cmdDir}/${obj.tmplName}`);
+process.chdir(`${cwd}/src/${cmdDir}/${obj.tmplName}`);
 
 fs.writeFileSync('index.js', indexTmpl);
 fs.writeFileSync('index.scss', scssTmpl);
 
-let sDir = `${cwd}/src/store`
-console.log(`${dir}/${fileName}${dirName}模版已创建`);
-if (isStore == 's') {
-  process.chdir(sDir);
-  fs.writeFileSync(`${dirName}.js`, storeTmpl);
-  console.log(`store/${dirName}已创建`);
+if (isStore) {
+  let storeDir = `${cwd}/src/store`
+  process.chdir(storeDir);
+  fs.writeFileSync(`${tmplName}.js`, storeTmpl);
 }
-
+console.log('--------------------------------------\n');
+console.log(`${cmdDir}/${obj.tmplName}模版已创建`);
+isStore && console.log(`store/${tmplName}已创建`);
+console.log('--------------------------------------\n');
 function titleCase (str) {
-  const array = str.toLowerCase().split(' ');
-  for (let i = 0; i < array.length; i++) {
-    array[i] = array[i][0].toUpperCase() + array[i].substring(1, array[i].length);
+  const _arr = str.toLowerCase().split(' ');
+  for (let i = 0; i < _arr.length; i++) {
+    _arr[i] = _arr[i][0].toUpperCase() + _arr[i].substring(1, _arr[i].length);
   }
-  const string = array.join(' ');
-  return string;
+  const _str = _arr.join(' ');
+  return _str;
 }
 
 process.exit(0);
